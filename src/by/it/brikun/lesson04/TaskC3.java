63c03676b6cf3b7b44f6dd64bdd1744ae235f9b0
mac <y.brikun@tut.by>
2018-09-28 11:18:41 +0300

    lesson2-4 minus c2(less 02)  done

--- src/by/it/brikun/lesson04/TaskC3.java
+++ src/by/it/brikun/lesson04/TaskC3.java
@@ -24,5 +24,77 @@ package by.it.brikun.lesson04;
 */
 
 public class TaskC3 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10; i++) {
+            for (int j = 2; j <= 10; j++) {
+                System.out.println(build(i) + " умножить на " + build(j) + " равно " + build(i * j));
+            }
+        }
+        System.out.println(build(50));
+    }
 
+    static String build(int i) {
+        if (i < 20) {
+            switch (i) {
+                case 0:
+                    return "ноль";
+                case 1:
+                    return "один";
+                case 2:
+                    return "два";
+                case 3:
+                    return "три";
+                case 4:
+                    return "четыре";
+                case 5:
+                    return "пять";
+                case 6:
+                    return "шесть";
+                case 7:
+                    return "семь";
+                case 8:
+                    return "восемь";
+                case 9:
+                    return "девять";
+                case 10:
+                    return "десять";
+                case 11:
+                    return "одиннадцать";
+                case 12:
+                    return "двенадцать";
+                case 13:
+                    return "тринадцать";
+                case 14:
+                    return "четырнадцать";
+                case 15:
+                    return "пятнадцать";
+                case 16:
+                    return "шестнадцать";
+                case 17:
+                    return "семнадцать";
+                case 18:
+                    return "восемнадцать";
+                case 19:
+                    return "девятнадцать";
+            }
+        }
+        String end;
+        if ((i % 10) > 0)
+            end = " " + build(i % 10);
+        else
+            end = "";
+        switch (i / 10) {
+            case 2: return "двадцать" + end;
+            case 3: return "тридцать" + end;
+            case 4: return "сорок" + end;
+            case 5: return "пятьдесят" + end;
+            case 6: return "шестьдесят" + end;
+            case 7: return "семьдесят" + end;
+            case 8: return "восемьдесят" + end;
+            case 9: return "девяносто" + end;
+            case 10:return "сто" + end;
+        }
+        return "Слишком много";
+
+    }
 }
--- src/by/it/brikun/lesson05/TaskB1.java
+++ src/by/it/brikun/lesson05/TaskB1.java
@@ -10,6 +10,38 @@ package by.it.brikun.lesson05;
 */
 
 
-public class TaskB1 {
+import java.io.IOError;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
+public class TaskB1 {
+    public static void main(String[] args) throws IOException {
+        ArrayList list1=new ArrayList();
+        Collections.addAll(list1,1,2,3);
+        ArrayList list2=new ArrayList();
+        Collections.addAll(list2,4,5,6);
+        ArrayList list3=new ArrayList();
+        Collections.addAll(list3,7,8,9);
+        ArrayList list4=new ArrayList();
+        Collections.addAll(list4,10,11,12);
+        ArrayList list5=new ArrayList();
+        Collections.addAll(list5,13,14,15);
+        int sum;
+        sum= list1.size()+list2.size()+list3.size()+list4.size()+list5.size();
+        System.out.println(sum);
+      /*
+        for (int x[]:list1) {
+        for (int y:x) {
+        System.out.println(y);
+        }
+        }
+        */
+
+        list1.add(list1);
+        System.out.println(list1);
 }
+        //System.out.println(list1);
+    }
+//}
